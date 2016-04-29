763f1b55e2d32b6e9572c2c431252e722f78ac88
Ellon Mendes <ellon.mendes@laas.fr>
2016-04-29 10:39:29 +0200

    Add SurfaceNormalDataPointsFilter to paraview plugin

---
+++ vtkPMSurfaceNormalDataPointsFilter.cxx
@@ -0,0 +1,90 @@
+/*=========================================================================
+
+  Program:   Lib Point Matcher Plugin for ParaView
+  Module:    vtkPMSurfaceNormalDataPointsFilter.cxx
+
+  Copyright (c) Ellon Paiva Mendes
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+  
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkPMSurfaceNormalDataPointsFilter.h"
+#include "vtkPMConversions.h"
+
+#include "vtkPolyData.h"
+#include "vtkPointData.h"
+#include "vtkInformation.h"
+#include "vtkInformationVector.h"
+#include "vtkObjectFactory.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+#include "vtkSmartPointer.h"
+#include "vtkNew.h"
+
+typedef vtkPMConversions::PM PM;
+
+//----------------------------------------------------------------------------
+vtkStandardNewMacro(vtkPMSurfaceNormalDataPointsFilter);
+
+//----------------------------------------------------------------------------
+vtkPMSurfaceNormalDataPointsFilter::vtkPMSurfaceNormalDataPointsFilter()
+{
+  this->Knn = 5;
+  this->Epsilon = 0;
+  this->KeepNormals = 1;
+  this->KeepDensities = 0;
+  this->KeepEigenValues = 0;
+  this->KeepEigenVectors = 0;
+  this->KeepMatchedIds = 0;
+  this->SetNumberOfInputPorts(1);
+  this->SetNumberOfOutputPorts(1);
+}
+
+//----------------------------------------------------------------------------
+vtkPMSurfaceNormalDataPointsFilter::~vtkPMSurfaceNormalDataPointsFilter()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkPMSurfaceNormalDataPointsFilter::RequestData(
+  vtkInformation* vtkNotUsed(request),
+  vtkInformationVector **inputVector,
+  vtkInformationVector *outputVector)
+{
+  // get input and output data objects
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkPolyData *input = vtkPolyData::SafeDownCast(inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+  vtkPolyData *output = vtkPolyData::SafeDownCast(outInfo->Get(vtkDataObject::DATA_OBJECT()));
+
+  // configure the filter
+  PointMatcherSupport::Parametrizable::Parameters params;
+  std::string name = "SurfaceNormalDataPointsFilter";
+  params["knn"] = boost::lexical_cast<std::string>(this->Knn);
+  params["epsilon"] = boost::lexical_cast<std::string>(this->Epsilon);
+  params["keepNormals"] = boost::lexical_cast<std::string>(this->KeepNormals);
+  params["keepDensities"] = boost::lexical_cast<std::string>(this->KeepDensities);
+  params["keepEigenValues"] = boost::lexical_cast<std::string>(this->KeepEigenValues);
+  params["keepEigenVectors"] = boost::lexical_cast<std::string>(this->KeepEigenVectors);
+  params["keepMatchedIds"] = boost::lexical_cast<std::string>(this->KeepMatchedIds);
+  PM::DataPointsFilter* PM_filter = 
+    PM::get().DataPointsFilterRegistrar.create(name, params);
+
+  // perform fix step sampling
+  boost::shared_ptr<PM::DataPoints> cloud = vtkPMConversions::DataPointsFromPolyData(input);
+  PM_filter->inPlaceFilter(*cloud);
+
+  // store output
+  output->ShallowCopy(vtkPMConversions::PolyDataFromDataPoints(*cloud));
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkPMSurfaceNormalDataPointsFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ vtkPMSurfaceNormalDataPointsFilter.h
@@ -0,0 +1,79 @@
+/*=========================================================================
+
+  Program:   Lib Point Matcher Plugin for ParaView
+  Module:    vtkPMFixStepSamplingFilter.h
+
+  Copyright (c) Ellon Paiva Mendes
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+  
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkPMSurfaceNormalDataPointsFilter -
+// .SECTION Description
+//
+
+#ifndef __vtkPMSurfaceNormalDataPointsFilter_h
+#define __vtkPMSurfaceNormalDataPointsFilter_h
+
+#include <vtkPolyDataAlgorithm.h>
+#include <vtkPMFiltersModule.h>
+
+
+class  VTKPMFILTERS_EXPORT vtkPMSurfaceNormalDataPointsFilter : public vtkPolyDataAlgorithm
+{
+public:
+  vtkTypeMacro(vtkPMSurfaceNormalDataPointsFilter, vtkPolyDataAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  static vtkPMSurfaceNormalDataPointsFilter *New();
+
+  vtkSetMacro(Knn, unsigned);
+  vtkGetMacro(Knn, unsigned);
+
+  vtkSetMacro(Epsilon, double);
+  vtkGetMacro(Epsilon, double);
+
+  vtkSetMacro(KeepNormals, bool);
+  vtkGetMacro(KeepNormals, bool);
+
+  vtkSetMacro(KeepDensities, bool);
+  vtkGetMacro(KeepDensities, bool);
+
+  vtkSetMacro(KeepEigenValues, bool);
+  vtkGetMacro(KeepEigenValues, bool);
+
+  vtkSetMacro(KeepEigenVectors, bool);
+  vtkGetMacro(KeepEigenVectors, bool);
+
+  vtkSetMacro(KeepMatchedIds, bool);
+  vtkGetMacro(KeepMatchedIds, bool);
+
+protected:
+
+  unsigned Knn;
+  double Epsilon;
+  bool KeepNormals;
+  bool KeepDensities;
+  bool KeepEigenValues;
+  bool KeepEigenVectors;
+  bool KeepMatchedIds;
+
+  virtual int RequestData(vtkInformation *request,
+                          vtkInformationVector **inputVector,
+                          vtkInformationVector *outputVector);
+
+  vtkPMSurfaceNormalDataPointsFilter();
+  virtual ~vtkPMSurfaceNormalDataPointsFilter();
+
+private:
+  vtkPMSurfaceNormalDataPointsFilter(const vtkPMSurfaceNormalDataPointsFilter&);  // Not implemented.
+  void operator=(const vtkPMSurfaceNormalDataPointsFilter&);  // Not implemented.
+};
+
+#endif
+
+
