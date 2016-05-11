138e0446d7a683d9babd666867842666299f5780
Ellon Mendes <ellon.mendes@laas.fr>
2016-05-11 18:19:26 +0200

    Add script to get the auto-generated filenames from the yaml file

---
+++ python/autogen_get_filenames.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# usage: ./generate_datafilters_source_files.py <filters.yaml> <h|cxx|xml> [base-directory]
+from __future__ import print_function
+import sys
+import os
+import yaml
+
+
+# Load filters config file as dict
+filters_config_filename = sys.argv[1]
+with open(filters_config_filename, 'r') as stream:
+    filter_dict = yaml.load(stream)
+
+# Get extension from arguments
+extension = sys.argv[2]
+
+# Get base directory if supplied
+if len(sys.argv) < 4:
+	base_dir = ''
+else:
+	base_dir = sys.argv[3]
+
+filenames = []
+for filterName in filter_dict:
+	filenames.append(os.path.join(base_dir,'vtkPM'+filterName+'.'+extension))
+
+print(';'.join(filenames), end="")
